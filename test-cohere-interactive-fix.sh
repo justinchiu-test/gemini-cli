b69074bb900379b897ab672f6f78b2e323d6b861
justinchiu-cohere <justinchiu@cohere.com>
2025-07-10 21:14:01 -0400

    remove testing scripts

--- test-cohere-interactive-fix.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Testing Cohere interactive mode with tool calls..."
-echo "Type 'run ls' when prompted and then 'exit' to quit"
-echo ""
-
-# Run in interactive mode with debug enabled
-npm run cli -- --provider cohere --debug -i
